9102680b97cbd934c092c1078ab1a6d239ea6f3d
ViktoriaGavrosh <viktoria.gavrosh@gmail.com>
2023-04-28 15:02:19 +0300

    +args, fix fillList

--- Main.kt
+++ Main.kt
@@ -2,18 +2,27 @@ package sorting
 
 import java.util.Scanner
 
-fun main() {
-    val list = fillList()
+fun main(args: Array<String>) {
+    val list =
+        try {
+            fillList(args[3])
+        } catch (e: Exception) {
+            fillList("word")
+        }
     println("Total numbers: ${list.size}.")
-    val counter = Counter(list)
+    /*val counter = Counter(list)
     val maxVal = counter.countMaxValue()
-    println("The greatest number: ${maxVal["max"]} (${maxVal["countMax"]} time(s)).")
+    println("The greatest number: ${maxVal["max"]} (${maxVal["countMax"]} time(s)).")*/
 }
 
-fun fillList(): MutableList<Any> {
+fun fillList(type: String): MutableList<Any> {
     val scan = Scanner(System.`in`)
     val list = mutableListOf<Any>()
-    while (scan.hasNext()) list.add(scan.next())
+    when(type) {
+        "long" -> while (scan.hasNextLong()) list.add(scan.nextLong())
+        "line" -> while (scan.hasNextLine()) list.add(scan.nextLine())
+        else -> while (scan.hasNext()) list.add(scan.next())
+    }
     return list
 }
 
